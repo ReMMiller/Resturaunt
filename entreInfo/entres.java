cb5effe05106fac3f4fcb2920cbb03690a4b192e
remiller <remiller@student.neumont.edu>
2018-08-03 09:28:17 -0600

    yupp

---
+++ entreInfo/entres.java
@@ -0,0 +1,29 @@
+Menu Entrees::
+		
+		Crab Cake Special $15.99
+			Four crispy, well-broiled crab cakes made from local brown crab meat.
+			
+		Shrimp Scampi $13.99
+			Fresh, local pink shrimp saute'ed to perfection in a garlic/olive oil sauce and laid out on 
+			flour spiggetti.
+				
+		Fried Shrimp $11.99
+			Fresh, local pink shrimp lightly fried in a bourbon batter.
+			
+		Soft-Shell Crab $13.99
+			Local brown crab saute'ed lightly in a butter-garlic marinade.
+			
+		Crab Imperial $15.99
+			Local, well-seasoned brown crab meat mixed with mayonnaise and cheese, served in a scallop shell.
+			
+		Baked Stuffed Flounder with Crab $15.99
+			A crab meat-mix served inside a baked flouder fillet and sprinkled with butter and herbs.
+			
+		Grilled Shrimp Skewers $12.99
+			Whole shrimp grilled and served on skewers with peppers and cucumbers.
+			
+		Fried Flounder $11.99
+			Crispy, lightly pan-fried flouder fillets showered in lemon-onion sauce.
+			
+		Grilled Salmon $12.99
+			Fresh salmon filles in brown sugar marinade well-grilled.
